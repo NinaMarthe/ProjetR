c296420c3a91a89359b29b0b8751867561b19d7a
ElyesBraham <96091237+ElyesBraham@users.noreply.github.com>
2022-03-14 14:31:01 +0100

    Update app.R

--- app.R
+++ app.R
@@ -2,73 +2,62 @@ library(shiny)
 library(shinydashboard)
 library(shinythemes)
 library(shinyMatrix)
+source("C:/Users/33645/Documents/R/ProjetR/generation_grille.R")
+source("C:/Users/33645/Documents/R/ProjetR/fonctions.R")
+source("C:/Users/33645/Documents/R/ProjetR/solver.R")
 
 ##########################################################
 ###################### UI.R #########################
 ##########################################################
 
 if (interactive()) {
-  
+
   ui <- fluidPage(
-    
+
     titlePanel("Sudoku"),
-    
+
     dashboardPage(
       dashboardHeader(),
       dashboardSidebar(
-        sidebarMenu(        
+        sidebarMenu(
           radioButtons(inputId="radio",  #radio pour ne choisir qu'une difficulté à la fois
                        label ="Difficulté",
-                       choices = c("Facile" = "easy", 
-                                   "Moyen" = "medium", 
+                       choices = c("Facile" = "easy",
+                                   "Moyen" = "medium",
                                    "Difficile" = "hard"),
                        selected = "easy"),
           hr(),
-          
+
           actionButton(inputId="load", label="Générer un nouveau Sudoku", icon = icon("cog")),
           hr(),
           actionButton(inputId="solve", label="Solution", icon = icon("check")),
           hr(),
-          
-          #input numéro ? 
-          #  selectInput("row", label = "Row", 
-          #              choices = list(1,2,3,4,5,6,7,8,9), 
-          #              selected = 1),
-          #  selectInput("col", label = "Column", 
-          #              choices = list(1,2,3,4,5,6,7,8,9), 
-          #              selected = 1),     
-          #  selectInput("value", label = "Value", 
-          #              choices = list(1,2,3,4,5,6,7,8,9," "=0), 
-          #              selected = 1),
-          #   actionButton("setButton", "Set"),
-          #   hr()
-          
+
           menuItem("", icon = icon(""))
-          
+
         )),
       dashboardBody(  theme = shinytheme('superhero'),
                       selectInput('grille',label = '', choices = c(9)),
-                      #  colorInput() ou data table https://onesixx.com/matrix-ggplot/
                       actionButton("add", "Jouer !"),
                       uiOutput("grid"),
-                      
+
                       mainPanel(
                         plotOutput("plot_sudoku"),
-                        plotOutput("plot__solve_sudoku")
-                        
+                        plotOutput("plot_solve_sudoku")
+
                       )
-                      
+
       )))
-  
-  
-  
+
+
+
   ##########################################################
   ###################### Server.R #########################
   ##########################################################
-  
-  
+
+
   server <- function(input, output, session) {
-    
+
     # Création de la grille 9x9
     observe({
       if(!is.null(input$add)) {
@@ -76,29 +65,27 @@ if (interactive()) {
                               ncol = as.numeric(input$grille),
                               nrow = as.numeric(input$grille)) })
         output$grid <- renderUI({
-          div(matrixInput(inputId = "newGrid", value = m()),)
+          div(matrixInput(inputId = "grid", value = m()),)
         })
       }
     })
-    
-    #Générer un nouveau Sudoku
+
+    #Générer un nouveau Sudoku avec les valeurs manquantes
     observeEvent(input$load,{
-      sudoku <- grille() 
+      sudoku <- grille2(grille)
       output$plot_sudoku <- renderPlot({plot(sudoku)})
-      
-      #Générer un nouveau Sudoku selon la difficulté
-      #  level_sudoku <- level_sudoku_nina() # fonction a changer 
-      #  output$plot_diff <- renderPlot({plot(sudoku)})
     })
-    
+
+
     #Générer la solution du Sudoku
     observeEvent(input$solve,{
-      solution_sudoku <- solution_sudoku_nina # fonction a changer
-      output$plot_solve_sudoku <- renderPlot({plot.sudoku(solution_sudoku_nina)})
+      solution_sudoku <- sudoku_solver(grille2)
+      output$plot_solve_sudoku <- renderPlot({plot(solution_sudoku)})
+
     })
-    
-    
-    
+
+
+
   }
   shinyApp(ui, server)
 }
